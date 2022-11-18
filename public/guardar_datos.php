5f63a412c142e7b07ab153efc8e1fc643c765d43
DBulacio <daniel.bulacio@hotmail.com>
2022-11-18 00:33:06 -0300

    primera parte de guardado de etiquetas

--- public/guardar_datos.php
+++ public/guardar_datos.php
@@ -17,36 +17,53 @@ try {
     $categoria = $_POST['categoria'];
     
     # Select
-    $SELECT_region = $con->prepare("SELECT * FROM regiones WHERE regionID = :regionID");
+    $SELECT_regiones  = $con->prepare("SELECT * FROM regiones WHERE regionID = :regionID");
+    $SELECT_etiquetas = $con->prepare("SELECT * FROM etiquetas WHERE etiqueta = :etiqueta");
     # Insert
     $INSERT_publicacion = $con->prepare("INSERT INTO publicacion (contenido, categoria, hora) VALUES (:contenido, :categoria, :hora)");
     $INSERT_enlace      = $con->prepare("INSERT INTO enlace (publicacionID, enlace) VALUES (:publicacionID, :enlace)");
     $INSERT_etiqueta    = $con->prepare("INSERT INTO etiquetas (etiqueta) VALUES (:etiqueta)");
-    $INSERT_etiqueta_publi = $con->prepare("INSERT INTO publi_tags (publicacionID, regionID) VALUES (:publicacionID, :etiquetaID)");
-    $INSERT_region_publi   = $con->prepare("INSERT INTO publi_region (publicacionID, regionID) VALUES (:publicacionID, :regionID)");
+    $INSERT_publi_etiqueta = $con->prepare("INSERT INTO publi_tags (publicacionID, etiquetaID) VALUES (:publicacionID, :etiquetaID)");
+    $INSERT_publi_regiones = $con->prepare("INSERT INTO publi_region (publicacionID, regionID) VALUES (:publicacionID, :regionID)");
     
     $INSERT_publicacion->execute(['contenido' => $contenido, 'categoria' => $categoria, 'hora' => $hora]);
     $publicacionID = $con->lastInsertId();
 
     if(isset($_POST['region'])) {
-        $SELECT_region->execute(['regionID' => $_POST['region']]);
-        while($row = $SELECT_region->fetch()) { 
-            $regionID = $row['regionID'];
+        $SELECT_regiones->execute(['regionID' => $_POST['region']]);
+        while($row_regiones = $SELECT_regiones->fetch()) { 
+            # Traer regionID
+            $regionID = $row_regiones['regionID'];
         }
-
-        $INSERT_region_publi->execute(['publicacionID' => $publicacionID, 'regionID' => $regionID]);
+        # Insertar en publi_region
+        $INSERT_publi_regiones->execute(['publicacionID' => $publicacionID, 'regionID' => $regionID]);
     }
     
+    if(isset($_POST['etiqueta'])) {
+        # Las etiquetas vienen separadas por comas. 
+        # Falta proceso de separación y subida por separado.
+
+        # Chequear si la etiqueta existe en etiquetas
+        $SELECT_etiquetas->execute(['etiqueta' => $_POST['etiqueta']]);
+        if($SELECT_etiquetas->rowCount() > 0) { 
+            # Si existe traer el id
+            while($row_etiquetas = $SELECT_etiquetas->fetch()) {
+                $etiquetaID = $row_etiquetas['etiquetaID'];
+            }
+        } else {
+            # Si no existe crearla y traer el id
+            if($INSERT_etiqueta->execute(['etiqueta' => $_POST['etiqueta']]))
+                $etiquetaID = $con->lastInsertId();
+        }
+        # Insertar en publi_tags
+        $INSERT_publi_etiqueta->execute(['publicacionID' => $publicacionID, 'etiquetaID' => $etiquetaID]);
+    }
+
     # Pensar cómo hacerlo
     // if(isset($_POST['enlace'])) {
     //     $INSERT_enlace->execute('publicacionID' => $publicacionID, 'enlace' => $enlace); # Chequear que se haya hecho...
     // }
 
-    // if(isset($_POST['etiqueta'])) {
-    //     # Procesar las etiquetas separadas por comas.
-    //     # ...
-    //     $INSERT_etiqueta->execute('publicacionID' => $publicacionID, 'etiqueta' => $etiqueta); # Chequear que se haya hecho...
-    // }
 
     $con->commit();
     # Mensaje de ok! 
